05e732ff8b585361647d2e7b791e90cd97d4515c
Carlos Carrete <carloscarrete,sc@gmail.com>
2024-06-20 19:06:35 -0600

    first commit

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,61 @@
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { addHours } from 'date-fns'
+
+import { Navbar } from '../components/Navbar'
+import { localizer, getMessages } from '../../helpers'
+import { CalendarEvent } from '../components/CalendarEvent'
+
+
+
+const events = [{
+  title: 'Big Meeting',
+  allDay: true,
+  start: new Date(),
+  end: addHours(new Date(), 2),
+  bgColor: '#ffffff',
+  user: {
+    _id: 'c15164',
+    name: 'Carlos',
+  }
+}]
+
+export const CalendarPage = () => {
+
+  const evetStyleGetter = (event, start, end, isSelected) => {
+
+    const style = {
+      backgroundColor: 'red',
+      borderRadius: '0px',
+      opacity: 0.8,
+      color: 'white',
+      border: '0px',
+    }
+
+
+    return {
+      style
+    }
+  }
+
+  return (
+    <>
+      <Navbar />
+      <Calendar
+        culture='es'
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc(100vh - 80px)' }}
+        messages={getMessages()}
+        eventPropGetter={evetStyleGetter}
+        components={
+          {
+            event: CalendarEvent
+          }
+        }
+      />
+    </>
+  )
+}
